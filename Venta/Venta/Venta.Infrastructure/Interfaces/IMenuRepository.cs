d0f8fc76c0ae007c0c42d1cafb765237017ca94d
AngelJV <20212431@itla.edu.do>
2023-06-12 22:50:13 -0400

    correcion 1.5

--- Venta/Venta/Venta.Infrastructure/Interfaces/IMenuRepository.cs
+++ Venta/Venta/Venta.Infrastructure/Interfaces/IMenuRepository.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 using Venta.Domain.Entity;
+using Venta.Domain.Repository;
 
 namespace Venta.Infrastructure.Interfaces
 {
-    public interface IMenuRepository
+    public interface IMenuRepository: IRepositoryBase<Menu>
     {
-        List<Menu> GetMenuByMenuRol(int menurolId);
+        
     }
 }
--- Venta/Venta/Venta.Infrastructure/Interfaces/IMenuRolRepository.cs
+++ Venta/Venta/Venta.Infrastructure/Interfaces/IMenuRolRepository.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Venta.Domain.Entity;
+using Venta.Domain.Repository;
 
 namespace Venta.Infrastructure.Interfaces
 {
-    public interface IMenuRolRepository
+    public interface IMenuRolRepository: IRepositoryBase<MenuRol>
     {
     }
 }
